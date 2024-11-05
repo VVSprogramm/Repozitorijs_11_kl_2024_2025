ede4724d9d268c864cab1df62275be1b432cbd74
VVSprogramm <74134370+VVSprogramm@users.noreply.github.com>
2024-11-05 08:47:15 +0200

    Create OOP_start.py

---
+++ OOP_start.py
@@ -0,0 +1,50 @@
+#Objekt-orientēta programmēšana
+
+#Objekts
+#Klase
+
+#class - atslēgas vārds klases izveidei
+
+#īpašības
+
+class Transportlidzeklis():
+
+    #incializēšana
+    def __init__(self,krasa,riepu_skaits,brauksanas_atrums,vietu_skaits):
+        self.krasa = krasa
+        self.riepu_skaits = riepu_skaits
+        self.brauksanas_atrums = brauksanas_atrums
+        self.vietu_skaits = vietu_skaits
+
+    def dati(self):
+        pass
+
+bmw = Transportlidzeklis("Sarkana",4,240,5)
+print(bmw.riepu_skaits)
+
+
+#1.uzd.
+
+#Izveido klasi Putns ar īpašībām krāsa, vide, kur dzīvo, lido (jā,nē)
+
+class Putns():
+
+    def __init__(self,krasa,vide,lido):
+        self.krasa = krasa
+        self.vide = vide
+        self.lido = bool(lido)
+
+
+#2.uzd.
+
+#Izveido objektu (kādu piemēra puntu), piedēvē tam nepieciešamās īpašības
+
+Erglis = Putns("Melns","Mežš, jūras krasts",True)
+
+#3.uzd.
+
+#Izvadi visas trīs objekta īpašības, izmantojot OOP
+
+print(Erglis.krasa)
+print(Erglis.vide)
+print(Erglis.lido)
